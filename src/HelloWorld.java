1a19961e16cf56d0d2613499dad037f4a161e010
Luxray93 <john117654@gmail.com>
2018-09-05 16:17:34 +0200

    Quadrants

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
-}
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+			int x = input.nextInt();
+			int y = input.nextInt();
+			
+			if(x>0 && y>0){
+				System.out.println(1);
+			}
+			if(x>0 && y<0){
+				System.out.println(4);
+			}
+			if(x<0 && y<0){
+				System.out.println(3);
+			}
+			if(x<0 && y>0){
+				System.out.println(2);
+			}
+	
+	}// END main
+	
+	
+}// END HelloWorld
