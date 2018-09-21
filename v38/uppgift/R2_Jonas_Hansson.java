15a77de1b1dad7e2a7baba78745d10e62553e09c
Luxray93 <john117654@gmail.com>
2018-09-21 14:39:35 +0200

    R2 solution

---
+++ v38/uppgift/R2_Jonas_Hansson.java
@@ -0,0 +1,16 @@
+package uppgift;
+
+import java.util.Scanner;
+
+public class R2_Jonas_Hansson {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int r2;
+		int s = input.nextInt();
+		int r1 = input.nextInt();
+		
+		System.out.println( +r1*2-s);
+	}
+
+
+}
